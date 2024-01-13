e2388e4d96f621bc16a3f7d42ee10d0c2eea7417
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 02:26:19 +0500

    NODEJS Assignment Q2
    Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”

---
+++ Q2.js
@@ -0,0 +1,3 @@
+// 1.Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var Name = "Eric";
+console.log("Hello ".concat(Name, ", would you like to learn some typescript today?"));
